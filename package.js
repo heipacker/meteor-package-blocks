b1c8e472c5156cb061d638179e21dfca6c07acdc
heipacker <heipackermail@gmail.com>
2017-12-22 01:03:31 +0800

    new: change name from moac to heipacker for test

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'moac:blocks',
+  name: 'heipacker:blocks',
   summary: 'Provides informations about the current and last 50 blocks',
   version: '0.1.0',
   git: 'http://github.com/heipacker/meteor-package-blocks'
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.use('mongo', ['client', 'server']);
 
   // api.use('frozeman:persistent-minimongo@0.1.3', 'client');
-  api.use('moac:web3@0.1.0', ['client', 'server']);
+  api.use('heipacker:web3@0.1.0', ['client', 'server']);
 
   api.export(['McBlocks'], ['client', 'server']);
 
